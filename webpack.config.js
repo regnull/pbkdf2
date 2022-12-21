60082380fb848a6515edb5364efe04107fb2f8d6
Leonid Gorkin <regnull@gmail.com>
2022-12-20 20:12:42 -0500

    fix file name

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './browser',
+  entry: './browser.js',
   output: {
     path: path.resolve(__dirname),
     filename: './build/pbkdf2_wp.js'
